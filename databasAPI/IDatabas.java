802a862c6fb76b694ea7100f7d217dc39e017df7
Linnea64 <lb224ae@student.lnu.se>
2023-03-05 11:18:12 +0100

    LinneasBranch 05 11 18

--- databasAPI/IDatabas.java
+++ databasAPI/IDatabas.java
@@ -5,14 +5,14 @@ import java.util.Date;
 public interface IDatabas {
 
     /* Hämta alla böcker med riktig titel som inte är utlånade (alltså i samling men inte i lån).*/
- Bok[] hämtaTillgänglighet (String titel) throws SQLException;
+    Bok[] hämtaTillgänglighet (String titel);
 
     /*skapa en Lån och returnera en String
-    *
-    * behöver definiera vad databasen kan tänkas svara... för tester
-    *
-    */
-    String skapaLån (Date startDatum, int kontoID, int ISBN);
+     *
+     * behöver definiera vad databasen kan tänkas svara... för tester
+     *
+     */
+    String skapaLån (int kontoID, int ISBN);
 
 
     /*ta bort lån enligt medlems personnummer eller Bokens ISBN
@@ -22,7 +22,7 @@ public interface IDatabas {
      */
     String taBortLån(int kontoID, int ISBN);
 
-/*lägga till en medlem på svartlista*/
+    /*lägga till en medlem på svartlista*/
     /*svartlista medlem genom medlems personnummer
      *
      * behöver definiera vad databasen kan tänkas svara... för tester
@@ -33,31 +33,29 @@ public interface IDatabas {
 
     /*returnera String att Konto är skapad*/
     /*skapa Konto med medlemens namn, efternamn, personnummer och roll
-    *
-    * behöver definiera vad databasen kan tänkas svara... för tester
-    *
-    * */
+     *
+     * behöver definiera vad databasen kan tänkas svara... för tester
+     *
+     * */
     String skapaKonto(String fnamn, String  enamn, long personNr, String roll);
 
-
-    /* returnera en String efter att Konto är avslutad.
-    *
-    * behöver definiera vad databasen kan tänkas svara... för tester
-    *
-    */
-    String avslutaKonto(int kontoID);
-
-    /*  returnera en Konto[] objekt med alla konton på servern.
-    *
-    * */
-    Konto[] hämtaKonton();
 
     /* returnera en String efter att Konto är avslutad.
      *
      * behöver definiera vad databasen kan tänkas svara... för tester
      *
      */
-    public String registreraTempAvstänging(int kontoID);
+    String avslutaKonto(int kontoID);
+
+    /*  returnera en Konto[] objekt med alla konton på servern.
+     *
+     * */
+    Konto[] hämtaKonton();
+
+    public String registreraTempAvstänging(int kontoID, int numOfDays);
+
+    public String returnBook(int isbn);
+    public String updateAntalFörseningar(int kontoID);
+    public String updateAntalAvstängningar(int kontoID);
 
 }
-
