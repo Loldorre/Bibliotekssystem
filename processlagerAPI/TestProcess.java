b38cdde6408c96b5ddd0d7b664bdadd9531629e6
Viktor Sjögren <viktor@calcon.se>
2023-03-14 16:01:40 +0100

    mer test klara

--- processlagerAPI/TestProcess.java
+++ processlagerAPI/TestProcess.java
@@ -9,7 +9,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class TestProcess {
@@ -23,59 +26,53 @@ public class TestProcess {
 
     //Återställer mockobjektet inför varje test
     @BeforeEach
-
     void resetMock() {
         logger.debug("--Resetting mock--");
         reset(dbapi);
     }
     @Nested
-    @DisplayName("Test för tempAvstängning")
+    @DisplayName("Test för hämtaSamling")
     class hämtaSamling {
         @Test
-        @DisplayName("Samling hämtas ur databasen och ")
-        void testHämtaSamling1() throws Exception{
-        when(dbapi.hämtaBöcker()).thenReturn(new Bok[]{
-                new Bok(1,123456,"Dorians stora bruna","Dorian Jones",2010),
-                new Bok(2,123456,"Dorians stora bruna","Dorian Jones",2010),
-                new Bok(3,123456,"Dorians stora bruna","Dorian Jones",2010)
-        });
-
-        assertArrayEquals ( new String[]{
-                        "bokID: 1, Titel: Dorians stora bruna, Författare: Dorian Jones, Utgivningsår: 2010, ISBN: 123456,",
-                        "bokID: 2, Titel: Dorians stora bruna, Författare: Dorian Jones, Utgivningsår: 2010, ISBN: 123456,",
-                        "bokID: 3, Titel: Dorians stora bruna, Författare: Dorian Jones, Utgivningsår: 2010, ISBN: 123456,"
-        }, p.hämtaSamling()
-        );
-
-
+        @DisplayName("Samling hämtas ur databasen och info samlas i string array")
+        void testHämtaSamling1() throws Exception {
+            when(dbapi.hämtaBöcker()).thenReturn(new Bok[]{
+                    new Bok(1, 123456, "Dorians stora bruna", "Dorian Jones", 2010),
+                    new Bok(2, 222222, "Den andra stora bruna", "Dorian Jones", 2010),
+                    new Bok(3, 232323, "Nu är det brunt igen", "Dorian Jones", 2010)
+            });
+            assertArrayEquals(new String[]{
+                    "bokID: 1, Titel: Dorians stora bruna, Författare: Dorian Jones, Utgivningsår: 2010, ISBN: 123456,",
+                    "bokID: 2, Titel: Den andra stora bruna, Författare: Dorian Jones, Utgivningsår: 2010, ISBN: 222222,",
+                    "bokID: 3, Titel: Nu är det brunt igen, Författare: Dorian Jones, Utgivningsår: 2010, ISBN: 232323,"
+            }, p.hämtaSamling());
         }
+
+        @Test
+        @DisplayName("Samling hämtas ur databasen och den strular ()throws sql exception")
+        void testHämtaSamling2() throws SQLException {
+            when(dbapi.hämtaBöcker()).thenThrow(new SQLException());
+            assertThrows(SQLException.class, () -> p.hämtaSamling());
+        }
+    }
+    @Test
+    @DisplayName("Samling hämtas ur databasen och info samlas i string array")
+    void testHämtaSamling1() throws Exception {
+        when(dbapi.hämtaBöcker()).thenReturn(new Bok[]{
+                new Bok(1, 123456, "Dorians stora bruna", "Dorian Jones", 2010),
+                new Bok(2, 222222, "Den andra stora bruna", "Dorian Jones", 2010),
+                new Bok(3, 232323, "Nu är det brunt igen", "Dorian Jones", 2010)
+        });
+        assertArrayEquals(new String[]{
+                "bokID: 1, Titel: Dorians stora bruna, Författare: Dorian Jones, Utgivningsår: 2010, ISBN: 123456,",
+                "bokID: 2, Titel: Den andra stora bruna, Författare: Dorian Jones, Utgivningsår: 2010, ISBN: 222222,",
+                "bokID: 3, Titel: Nu är det brunt igen, Författare: Dorian Jones, Utgivningsår: 2010, ISBN: 232323,"
+        }, p.hämtaSamling());
     }
 
     @Nested
     @DisplayName("Test för svartlistaMedlem")
     class svartlistaMedlem {
-
-    }
-
-    @Nested
-    @DisplayName("Test för regKonto")
-    class regKonto {
-
-    }
-
-    @Nested
-    @DisplayName("Test för avslutaKonto")
-    class avslutaKonto {
-
-    }
-    @Nested
-    @DisplayName("Test för registreraLån")
-    class registreraLån {
-
-    }
-    @Nested
-    @DisplayName("Test för återlämnaBok")
-    class återlämnaBok {
 
     }
 }
